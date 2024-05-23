6f6f7504b4c7abfd4dd9a637fc563072c48ac8c0
Lambaon <vborkonyuk@gmail.com>
2024-05-23 22:10:12 +0200

    RandomNumbersGenerator

--- ConsoleApparsik/Program.cs
+++ ConsoleApparsik/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RandomNumbersGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Min");
+            int min = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Max");
+            int max = int.Parse(Console.ReadLine());
+
+            if (min > max)
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+            Random random = new Random();
+            
+            int[] numbers = new int[40];
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(min, max + 1);
+            }
+            
+            Array.Sort(numbers);
+
+            string output = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                output += numbers[i];
+                if (i < numbers.Length - 1)
+                {
+                    output += ", ";
+                }
+            }
+            
+            Console.WriteLine("Numbers");
+            Console.WriteLine(output);
+
+
+        }
+    }
+}
